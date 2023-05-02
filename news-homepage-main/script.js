35b547fb3143f391d34874b38d3c39179068fbef
TJ <xxterrelljacksonxx@gmail.com>
2023-05-02 17:52:42 -0400

    added transitions to burger menu

--- news-homepage-main/script.js
+++ news-homepage-main/script.js
@@ -1,6 +1,6 @@
 const menuBtn = Array.from(document.querySelectorAll(".menu-btn"));
 const header = document.querySelector("header");
-const nav = document.querySelector(".nav")
+const nav = document.querySelector(".nav ul")
 
 for (btn in menuBtn) {
   menuBtn[btn].onclick = toggleMenu;
@@ -8,4 +8,5 @@ for (btn in menuBtn) {
 
 function toggleMenu() {
     header.classList.toggle("show-menu");
+    nav.classList.toggle("active")
   }
